5db70b9a0809188efdf456717cab0fb869efc008
andeek <andee.kaplan@gmail.com>
2014-04-20 13:06:19 -0500

    prev/next buttons

--- _site/assets/javascripts/active_toggle.js
+++ _site/assets/javascripts/active_toggle.js
@@ -6,5 +6,5 @@ function set_visible() {
 
 	//set only active to visible
 	$('#main article').css("display", "none");
-	$('#main article[id="'+act+'"]').toggle(400);
+	$('#main article[id="'+act+'"]').toggle("fast");
 }
