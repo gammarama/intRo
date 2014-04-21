41acd0b3e67b2f7e72aed84cf3aab82924e31a7d
andeek <andee.kaplan@gmail.com>
2014-04-21 18:48:20 -0500

    blargh

--- assets/javascripts/next_prev.js
+++ assets/javascripts/next_prev.js
@@ -5,7 +5,7 @@ function manageNextPrev() {
 	//get active link
 	var act = $('#sidebar .active a').attr("href");
 	act = act.substring(1, act.length);
-
+  console.log(act)
 	//set only active to visible
 	/*$('#main article').css("display", "none");
 	$('#main article[id="'+act+'"]').toggle("fast");*/
