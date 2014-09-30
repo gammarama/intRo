38395767342a4aab7847db7734ca0c2d592b95a6
Eric Hare <ericrhare@gmail.com>
2014-09-30 13:51:51 -0500

    Remove the dumb header stuff

--- server.R
+++ server.R
@@ -1,10 +1,3 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-# 
-# http://www.rstudio.com/shiny/
-#
-
 library(shiny)
 library(ggplot2)
 library(ggvis)
