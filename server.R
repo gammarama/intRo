8683d4099158589efd875d8ee96538218bcc7813
Eric Hare <ericrhare@gmail.com>
2014-10-21 09:28:35 -0500

    Kind of make numeric summaries work is

--- server.R
+++ server.R
@@ -239,12 +239,14 @@ shinyServer(function(input, output, session) {
         } else if (input$grouping == "none") {
             return(summary(intro.data()[,input$tblvars]))
         } else {
-            dat <- intro.data()
-            dat$intro_grouping <- dat[,input$grouping]
+            dat <- intro.data()[,input$tblvars]
+            dat$intro_grouping <- intro.data()[,input$grouping]
             
-            dat.dplyr <- dat %>% group_by(intro_grouping) %>% summarise_each(funs(min, q1, median, q3, max, mean, sd))
+            dat.dplyr <- dat[,names(dat) %in% c("intro_grouping", numericNames(dat))] %>% group_by(intro_grouping) %>% summarise_each(funs(min, q1, median, q3, max, mean, sd))
+            dat.dplyr.df <- as.data.frame(dat.dplyr)
+            names(dat.dplyr.df)[1] <- input$grouping
             
-            return(dat.dplyr)
+            return(dat.dplyr.df)
         }
     })
     
