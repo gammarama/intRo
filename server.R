a0e812622d82e80cd52ee9e9913e99487c05cef0
Eric Hare <ericrhare@gmail.com>
2014-10-29 13:48:39 -0500

    Axes min/max working again

--- server.R
+++ server.R
@@ -333,39 +333,63 @@ shinyServer(function(input, output, session) {
         return(mydat)
     })
     
+    input_binwidth <- reactive({
+        if (is.na(input$binwidth)) NULL
+        else input$binwidth
+    })
+    
+    input_xdomain <- reactive({
+        return(c(input$xmin, input$xmax))
+    })
+    
+    input_ydomain <- reactive({
+        return(c(input$ymin, input$ymax))
+    })
+    
    intro.plot %>%
         ggvis(x = ~intro_x_num, y = ~intro_y_num) %>%
-        layer_histograms(width = input_slider(1, 20, 1, label = "Bin Width")) %>% 
-        bind_shiny("histogram", "histogram_ui")  
+        layer_histograms(width = input_binwidth) %>% 
+        scale_numeric("x", domain = input_xdomain, clamp = TRUE, nice = TRUE) %>%
+        scale_numeric("y", domain = input_ydomain, clamp = TRUE, nice = TRUE) %>%
+        bind_shiny("histogram")
 
     intro.quant %>%
         ggvis(x = ~intro_quant, y = ~intro_x_num) %>%
-        layer_points() %>%
+        layer_points() %>% 
+        scale_numeric("x", domain = input_xdomain, clamp = TRUE, nice = TRUE) %>%
+        scale_numeric("y", domain = input_ydomain, clamp = TRUE, nice = TRUE) %>%
         bind_shiny("quantileplot")
     
     intro.plot %>%
         ggvis(x = ~intro_x_num, y = ~intro_y_num) %>%
-        layer_points() %>%
+        layer_points() %>% 
+        scale_numeric("x", domain = input_xdomain, clamp = TRUE, nice = TRUE) %>%
+        scale_numeric("y", domain = input_ydomain, clamp = TRUE, nice = TRUE) %>%
         bind_shiny("scatterplot")
     
     intro.plot %>%
         ggvis(x = ~intro_x_num, y = ~intro_y_num) %>%
-        layer_lines() %>%
+        layer_lines() %>% 
+        scale_numeric("x", domain = input_xdomain, clamp = TRUE, nice = TRUE) %>%
+        scale_numeric("y", domain = input_ydomain, clamp = TRUE, nice = TRUE) %>%
         bind_shiny("linechart")
     
     intro.plot %>%
         ggvis(x = ~intro_x_cat, y = ~intro_y_num) %>%
-        layer_boxplots() %>%
+        layer_boxplots() %>% 
+        scale_numeric("y", domain = input_ydomain, clamp = TRUE, nice = TRUE) %>%
         bind_shiny("boxplot")
     
     intro.plot %>%
         ggvis(x = ~intro_x_cat, y = ~intro_y_num) %>%
         layer_bars() %>%
+        scale_numeric("y", domain = input_ydomain, clamp = TRUE, nice = TRUE) %>%
         bind_shiny("barchart")
     
     intro.plot %>%
         ggvis(x = ~intro_x_cat, y = ~intro_y_num) %>%
         layer_bars() %>%
+        scale_numeric("y", domain = input_ydomain, clamp = TRUE, nice = TRUE) %>%
         bind_shiny("paretochart")
     
     reg.data <- reactive({
