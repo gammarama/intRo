789d90127774829a3610240ba85c49c1df8552f6
Eric Hare <ericrhare@gmail.com>
2014-11-25 10:11:31 -0800

    Getting there with regression code printing

--- modules/regression/helper.R
+++ modules/regression/helper.R
@@ -2,18 +2,20 @@ r <- function (intro.data, x, y) {
     if (!(x %in% names(intro.data)) | !(y %in% names(intro.data))) return(NULL)
     if (!is.numeric(intro.data[,x]) | !is.numeric(intro.data[,y])) return(NULL)
     
-    return(paste("r =", round(cor(intro.data[,y], intro.data[,x], use = "complete.obs"), digits = 4)))
+    cat_and_eval("r_result <- paste('r =', round(cor(intro.data[,y], intro.data[,x], use = 'complete.obs'), digits = 4))", env = environment(), file = "code_regression.R", append = TRUE)
+    cat_and_eval("r_result", env = environment(), file = "code_regression.R", append = TRUE)
 }
 
 r2 <- function (intro.regression) {
-    return(paste("R^2 =", round(summary(intro.regression)$r.squared, digits = 4)))
+    cat_and_eval("r2_result <- paste('R^2 =', round(summary(intro.regression)$r.squared, digits = 4))", env = environment(), file = "code_regression.R", append = TRUE)
+    cat_and_eval("r2_result", env = environment(), file = "code_regression.R", append = TRUE)
 }
 
 tablereg <- function (intro.regression, x) {
-    tbl.fit <- coef(summary(intro.regression))
-    rownames(tbl.fit)[2] <- x
+    cat_and_eval("tbl.fit <- coef(summary(intro.regression))", env = environment(), file = "code_regression.R", append = TRUE)
+    cat_and_eval("rownames(tbl.fit)[2] <- x", env = environment(), file = "code_regression.R", append = TRUE)
     
-    return(tbl.fit)
+    cat_and_eval("tbl.fit", env = environment(), file = "code_regression.R", append = TRUE)
 }
 
 savefit <- function (intro.data, intro.regression) { 
