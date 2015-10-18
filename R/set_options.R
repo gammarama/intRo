ec35fad2dab4eebe999b8697599a4f5487db351f
Eric Hare <ericrhare@gmail.com>
2015-10-18 10:20:31 -0500

    Required packages routine

--- R/set_options.R
+++ R/set_options.R
@@ -18,6 +18,17 @@ set_options <- function(path, enabled_modules, theme) {
                                                        "statistics/t_test")
     if (is.null(theme)) theme <- "united"
     
+    ## Routine to check packages
+    pkgs.required <- unique(unlist(lapply(enabled_modules, function(mod) {
+        lib.path <- file.path(path, "modules", mod, "libraries.R")
+        lib.lines <- readLines(lib.path)
+        
+        gsub(".*library\\((.*?)\\).*", "\\1", lib.lines)
+    })))
+        
+    test <- which(!(pkgs.required %in% installed.packages()[,1]))
+    if (length(test) > 0) stop("One or more required packages missing: ", paste(pkgs.required[test], collapse = ", "))
+
     intRo_configuration <- list(intRo_enabled_modules = enabled_modules,
                                 intRo_theme = theme)
     save(intRo_configuration, file = file.path(path, "data", "configuration.rda"))
