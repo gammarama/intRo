b356b2e906b03ca22710811b2052b6d96b206d90
Eric Hare <ericrhare@gmail.com>
2016-01-08 10:09:27 -0600

    Reference the correct inputs in contingency

--- modules/statistics/contingency/reactive.R
+++ modules/statistics/contingency/reactive.R
@@ -1,6 +1,6 @@
 intro.contingency <- reactive({
     if (is.null(intro.data())) return(NULL)
-    if (input$xreg == input$yreg || length(categoricNames(intro.data())) < 2) return(NULL)
+    if (input$xcont == input$ycont || length(categoricNames(intro.data())) < 2) return(NULL)
     
     my.conttable <- cont.table(intro.data(), input$xcont, input$ycont, input$conttype, input$contdigits)
     
