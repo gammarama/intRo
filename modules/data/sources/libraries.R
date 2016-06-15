878b4bf7d6d953b34bd4603e2e2ecd6145854b4c
Eric Hare <ericrhare@gmail.com>
2016-06-15 16:29:56 -0500

    Load ggplot2 in the data module

--- modules/data/sources/libraries.R
+++ modules/data/sources/libraries.R
@@ -1,3 +1,4 @@
 if (!("package:dplyr" %in% search())) interpolate(~(library(dplyr)), file = "code_All.R", mydir = userdir, append = TRUE, nodupes = TRUE)
 if (!("package:lubridate" %in% search())) interpolate(~(library(lubridate)), file = "code_All.R", mydir = userdir, append = TRUE, nodupes = TRUE)
 if (!("package:YaleToolkit" %in% search())) interpolate(~(library(YaleToolkit)), file = "code_All.R", mydir = userdir, append = TRUE, nodupes = TRUE)
+if (!("package:ggplot2" %in% search())) interpolate(~(library(ggplot2)), file = "code_All.R", mydir = userdir, append = TRUE, nodupes = TRUE)
